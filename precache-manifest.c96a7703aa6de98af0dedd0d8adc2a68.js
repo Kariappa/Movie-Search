aa42a8c43629bbddeff8dd8b4bc373366c785f65
Ali Kariapper <kariappa@mcmaster.ca>
2020-08-05 11:51:34 -0400

    Updates

---
+++ precache-manifest.c96a7703aa6de98af0dedd0d8adc2a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6531d303ab4b8695955f69d51af65e7f",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "0e3c16bd687366fe4701",
+    "url": "/Movie-Search/static/css/main.65fb4fc7.chunk.css"
+  },
+  {
+    "revision": "4b6cc055889346c6d43c",
+    "url": "/Movie-Search/static/js/2.59e20c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search/static/js/2.59e20c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3c16bd687366fe4701",
+    "url": "/Movie-Search/static/js/main.e200c090.chunk.js"
+  },
+  {
+    "revision": "654f0821e1d6f29506b8",
+    "url": "/Movie-Search/static/js/runtime-main.ec789e46.js"
+  },
+  {
+    "revision": "ed9ce0a2727323e84c35891d00dbb7ea",
+    "url": "/Movie-Search/static/media/Bungee-Regular.ed9ce0a2.ttf"
+  }
+]);
